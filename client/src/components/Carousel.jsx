0c61bf1029122182f9330ee2467c2bb4cf9a6a07
BennyBlanco88 <ben.langs@yahoo.com>
2024-03-11 21:42:25 -0700

    fixed 2

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,9 +1,9 @@
 import Carousel from 'react-bootstrap/Carousel';
 import Profile1 from '../assets/profile1.png';
-import Profile2 from '../assets/Profile2.png';
-import Profile3 from '../assets/Profile3.png';
-import Profile4 from '../assets/Profile4.png';
-import Profile5 from '../assets/Profile5.png';
+import Profile2 from '../assets/profile2.png';
+import Profile3 from '../assets/profile3.png';
+import Profile4 from '../assets/profile4.png';
+import Profile5 from '../assets/profile5.png';
 
 function CarouselFadeExample() {
   return (
